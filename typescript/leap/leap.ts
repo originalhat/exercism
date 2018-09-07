506e63ba44dcd362429f864daad441bf981cda20
Devin Brown <devin.g.brown@gmail.com>
2018-09-07 08:30:30 -0600

    less code is more; ternary for life

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,9 +1,3 @@
-function isLeapYear(year: number): boolean {
-  if (year % 100 !== 0) {
-    return year % 4 === 0;
-  } else {
-    return year % 400 === 0;
-  }
+export default function isLeapYear(year: number): boolean {
+    return year % 100 !== 0 ? year % 4 === 0 : year % 400 === 0;
 }
-
-export default isLeapYear
