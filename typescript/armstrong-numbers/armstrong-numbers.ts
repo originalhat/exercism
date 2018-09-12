8c78c1dfdf6ff64b349bd2b2ab82ca08bea1fc31
Devin Brown <devin.g.brown@gmail.com>
2018-09-12 08:31:37 -0600

    inline

--- typescript/armstrong-numbers/armstrong-numbers.ts
+++ typescript/armstrong-numbers/armstrong-numbers.ts
@@ -1,11 +1,7 @@
 export default class ArmstrongNumbers {
   static isArmstrongNumber(number: number): boolean {
-    let numberAsArray = this.getNumberAsArray(number);
+    let numberAsArray = Array.from(number.toString()).map(n => parseInt(n));
 
     return numberAsArray.map(n => n ** numberAsArray.length).reduce((acc, cur) =>  acc + cur) === number;
-  }
-
-  private static getNumberAsArray(number: number): number[] {
-    return Array.from(number.toString()).map(n => parseInt(n));
   }
 }
