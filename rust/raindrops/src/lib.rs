c401d8e3bda467be520ae78e8a84a4d5115fb58c
Devin Brown <devin.g.brown@gmail.com>
2018-12-15 09:10:47 -0700

    made a rust baby

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut sound = String::from("");
+
+    if n % 3 == 0 || n % 5 == 0 || n % 7 == 0 {
+        if n % 3 == 0 {
+            sound.push_str("Pling")
+        }
+        if n % 5 == 0 {
+            sound.push_str("Plang");
+        }
+        if n % 7 == 0 {
+            sound.push_str("Plong");
+        }
+    } else {
+        sound = n.to_string();
+    }
+
+    return sound;
 }
