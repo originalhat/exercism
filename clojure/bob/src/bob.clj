11b515b76068a4499a31a5c550d3b1e7f053dd41
Devin Brown <devin.g.brown@gmail.com>
2019-07-30 09:32:00 -0600

    wip; not matching questions properly
    This should match anything with a question mark but for some reason is
    falling through to the else case.

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,12 +1,10 @@
 (ns bob
   (:require [clojure.string :as s]))
 
-;; TODO: is upper case? don't match on #'s or special characters
-;; (every? #(Character/isUpperCase))
-
 (defn response-for
   [sentence]
   (cond
     (= 0 (count (s/trim sentence))) "Fine. Be that way!"
     (every? #(Character/isUpperCase %) (clojure.string/replace sentence #"[^\p{L}]" "")) "Whoa, chill out!"
+    (s/includes? sentence "?") "Sure."
     :else "Whatever."))
