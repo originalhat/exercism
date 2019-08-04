95a4c9b20e12073d9fcf325dc6340699cab9a091
Devin Brown <devin.g.brown@gmail.com>
2019-08-04 13:06:20 -0600

    refactor to functions for clarity

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,20 +1,27 @@
 (ns bob
   (:require [clojure.string :as s]))
 
-(defn shouting?
-  "Returns true if the entire sentence is in caps, save special chars"
+(defn- is-shouting?
+  "Returns true if the entire sentence is in caps, save special chars."
   [sentence]
   (every?
     #(Character/isUpperCase %)
     (clojure.string/replace sentence #"[^\p{L}]" "")))
 
+(defn- is-quiet?
+  "Returns true if there is no response."
+  [sentence]
+  (= 0 (count (s/trim sentence))))
+
+(defn- is-question?
+  "Returns true if sentence is a question."
+  [sentence]
+  (s/ends-with? sentence "?"))
+
 (defn response-for
   [sentence]
-  (let [is-question? (s/ends-with? sentence "?")
-        is-quiet? (= 0 (count (s/trim sentence)))
-        is-shouting? (shouting? sentence)]
-    (cond
-      is-quiet? "Fine. Be that way!"
-      is-shouting? "Whoa, chill out!"
-      is-question? "Sure."
-      :else "Whatever.")))
+  (cond
+    (is-quiet? sentence) "Fine. Be that way!"
+    (is-shouting? sentence) "Whoa, chill out!"
+    (is-question? sentence) "Sure."
+    :else "Whatever."))
