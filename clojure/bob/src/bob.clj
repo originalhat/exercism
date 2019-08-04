331c3b1ad99ebec6ef5c435526ca54ff932d780e
Devin Brown <devin.g.brown@gmail.com>
2019-08-04 12:55:56 -0600

    refactor to vars/defs with improved names

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,10 +1,20 @@
 (ns bob
   (:require [clojure.string :as s]))
 
+(defn shouting?
+  "Returns true if the entire sentence is in caps, save special chars"
+  [sentence]
+  (every?
+    #(Character/isUpperCase %)
+    (clojure.string/replace sentence #"[^\p{L}]" "")))
+
 (defn response-for
   [sentence]
-  (cond
-    (= 0 (count (s/trim sentence))) "Fine. Be that way!"
-    (every? #(Character/isUpperCase %) (clojure.string/replace sentence #"[^\p{L}]" "")) "Whoa, chill out!"
-    (s/includes? sentence "?") "Sure."
-    :else "Whatever."))
+  (let [is-question? (s/ends-with? sentence "?")
+        is-quiet? (= 0 (count (s/trim sentence)))
+        is-shouting? (shouting? sentence)]
+    (cond
+      is-quiet? "Fine. Be that way!"
+      is-shouting? "Whoa, chill out!"
+      is-question? "Sure."
+      :else "Whatever.")))
