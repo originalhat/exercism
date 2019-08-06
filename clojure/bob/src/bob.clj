063c6868f2a46b60c3b747816dfa015c93b07b5c
Devin Brown <devin.g.brown@gmail.com>
2019-08-06 08:56:46 -0600

    still gross but a little better

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,37 +1,28 @@
 (ns bob
   (:require [clojure.string :as s]))
 
-(defn- is-shouting?
-  "Returns true if the entire sentence is in caps, save special chars."
-  [sentence]
+(defn- shouting?  [sentence]
   (every?
     #(Character/isUpperCase %)
     (s/replace sentence #"[^\p{L}]" "")))
 
 (defn- is-only-numbers?
-  "Returns true if the entire sentence is numeric, save special chars."
   [sentence]
   (every?
     #(Character/isDigit %)
     (s/replace sentence #"[^\p{L}|\d]" "")))
 
-(defn- is-quiet?
-  "Returns true if there is no response."
-  [sentence]
-  (= 0 (count (s/trim sentence))))
+(defn- silence?  [sentence] (s/blank? sentence))
 
-(defn- is-question?
-  "Returns true if sentence is a question."
-  [sentence]
-  (s/ends-with? sentence "?"))
+(defn- question?  [sentence] (s/ends-with? sentence "?"))
 
 (defn response-for
   [sentence]
   (cond
-    (is-quiet? sentence) "Fine. Be that way!"
-    (and (is-only-numbers? sentence) (is-question? sentence)) "Sure."
-    (and (is-question? sentence) (is-shouting? sentence)) "Calm down, I know what I'm doing!"
-    (is-question? sentence) "Sure."
+    (silence? sentence) "Fine. Be that way!"
+    (and (is-only-numbers? sentence) (question? sentence)) "Sure."
+    (and (question? sentence) (shouting? sentence)) "Calm down, I know what I'm doing!"
+    (question? sentence) "Sure."
     (is-only-numbers? sentence) "Whatever."
-    (is-shouting? sentence) "Whoa, chill out!"
+    (shouting? sentence) "Whoa, chill out!"
     :else "Whatever."))
