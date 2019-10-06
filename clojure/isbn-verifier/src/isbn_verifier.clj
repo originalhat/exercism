852f8caaec45483b8250c31898462081e594acd6
Devin Brown <devin.g.brown@gmail.com>
2019-10-06 12:17:54 -0600

    isbn cleanup within reason

--- clojure/isbn-verifier/src/isbn_verifier.clj
+++ clojure/isbn-verifier/src/isbn_verifier.clj
@@ -1,5 +1,24 @@
-(ns isbn-verifier)
+(ns isbn-verifier
+  (:require [clojure.string :as str]))
 
-(defn isbn? [isbn] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn- valid-isbn?
+  [processed-isbn]
+  (and (= 10 (count processed-isbn))
+       (some? (re-matches #"\d+" (str/join processed-isbn)))))
+
+(defn isbn? [isbn]
+  (let [processed-isbn (as-> isbn s
+                             (str/replace s #"-" "")
+                             (str/split s #"")
+                             (conj
+                               (vec (drop-last s))
+                               (str/replace (last s) #"X" "10"))
+                             (map read-string s)
+                             (reverse s)
+                             (vec s))]
+
+    (if (valid-isbn? processed-isbn)
+      (= 0 (mod (->> processed-isbn
+                     (map-indexed (fn [idx itm] (* (+ idx 1) itm)))
+                     (reduce +)) 11))
+      false)))
