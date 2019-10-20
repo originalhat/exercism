4bbbd36e79d883693d1c22bd153b68476b07ec07
Devin Brown <devin.g.brown@gmail.com>
2019-10-20 11:10:32 -0600

    anagram first pass

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,9 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn anagrams-for [word prospect-list]
+  (->> prospect-list
+       (filter #(not= (str/lower-case word) %))
+       (filter #(= (count word) (count %)))
+       (filter #(= (sort (re-seq #"\w" (str/lower-case word)))
+                   (sort (re-seq #"\w" (str/lower-case %)))))))
