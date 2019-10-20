e9d753304d1b82cb5c94c39c47c5a56d0b74074e
Devin Brown <devin.g.brown@gmail.com>
2019-10-20 11:43:27 -0600

    refactor: count is implied

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -4,6 +4,5 @@
 (defn anagrams-for [word prospect-list]
   (->> prospect-list
        (filter #(not= (str/lower-case word) %))
-       (filter #(= (count word) (count %)))
        (filter #(= (sort (str/lower-case word))
                    (sort (str/lower-case %))))))
