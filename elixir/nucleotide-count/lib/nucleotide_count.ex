314aa38023547695560cebdfbefd8275ee0f6064
Devin Brown <devin.g.brown@gmail.com>
2020-09-17 17:18:50 -0600

    nucleotide MVP

---
+++ elixir/nucleotide-count/lib/nucleotide_count.ex
@@ -0,0 +1,39 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide) do
+    strand
+    |> Enum.filter(fn seq -> seq == nucleotide end)
+    |> Enum.count()
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    %{
+      ?A => count(strand, ?A),
+      ?C => count(strand, ?C),
+      ?G => count(strand, ?G),
+      ?T => count(strand, ?T)
+    }
+  end
+end
