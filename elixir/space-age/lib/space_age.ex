72374ad4e367b8770f0f5ebf6907ca12a7a2ea9f
Devin Brown <devin.g.brown@gmail.com>
2020-09-23 08:20:37 -0600

    MAP keeps data separate from functionality

--- elixir/space-age/lib/space_age.ex
+++ elixir/space-age/lib/space_age.ex
@@ -11,17 +11,22 @@ defmodule SpaceAge do
 
   @seconds_in_earth_year 31_557_600
 
+  @second_ratio %{
+    :mercury => 0.2408467,
+    :venus => 0.61519726,
+    :earth => 1.0,
+    :mars => 1.8808158,
+    :jupiter => 11.862615,
+    :saturn => 29.447498,
+    :uranus => 84.016846,
+    :neptune => 164.79132,
+  }
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
   aged on 'planet'.
   """
   @spec age_on(planet, pos_integer) :: float
-  def age_on(:earth, seconds), do: seconds / @seconds_in_earth_year
-  def age_on(:mercury, seconds), do: age_on(:earth, seconds / 0.2408467)
-  def age_on(:venus, seconds), do: age_on(:earth, seconds / 0.61519726)
-  def age_on(:mars, seconds), do: age_on(:earth, seconds / 1.8808158)
-  def age_on(:jupiter, seconds), do: age_on(:earth, seconds / 11.862615)
-  def age_on(:saturn, seconds), do: age_on(:earth, seconds / 29.447498)
-  def age_on(:uranus, seconds), do: age_on(:earth, seconds / 84.016846)
-  def age_on(:neptune, seconds), do: age_on(:earth, seconds / 164.79132)
+  def age_on(planet, seconds) do
+    seconds / @seconds_in_earth_year / Map.fetch!(@second_ratio, planet)
+  end
 end
