f7fa414d8b7f5363b71a41f8714d0527f3470614
Devin Brown <devin.g.brown@gmail.com>
2021-02-12 07:05:26 -0700

    [Insert your commit message here. Be sure to make it descriptive.]

--- elixir/raindrops/lib/raindrops.ex
+++ elixir/raindrops/lib/raindrops.ex
@@ -10,5 +10,19 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t()
   def convert(number) do
+
+    # conditionally append strings
+
+    list = if factor?(number, 3), do: ["Pling"], else: []
+    list = if factor?(number, 5), do: list ++ ["Plang"], else: list
+    list = if factor?(number, 7), do: list ++ ["Plong"], else: list
+
+    if Enum.count(list) > 0 do
+      Enum.join(list)
+    else
+      Integer.to_string(number)
+    end
   end
+
+  defp factor?(n, factor), do: rem(n, factor) == 0
 end
