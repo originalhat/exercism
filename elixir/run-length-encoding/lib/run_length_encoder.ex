fb8f96ee6e9cfc6d2335d61f76256a2cb7d3557e
Devin Brown <devin.g.brown@gmail.com>
2021-02-14 12:18:56 -0700

    using similar approach to clojure; chunk/identity is your friend

--- elixir/run-length-encoding/lib/run_length_encoder.ex
+++ elixir/run-length-encoding/lib/run_length_encoder.ex
@@ -1,16 +1,35 @@
 defmodule RunLengthEncoder do
-  @doc """
-  Generates a string where consecutive elements are represented as a data value and count.
-  "AABBBCCCC" => "2A3B4C"
-  For this example, assume all input are strings, that are all uppercase letters.
-  It should also be able to reconstruct the data into its original form.
-  "2A3B4C" => "AABBBCCCC"
-  """
   @spec encode(String.t()) :: String.t()
-  def encode(string) do
+  def encode(string \\ "") do
+    string
+    |> String.graphemes()
+    |> Enum.chunk_by(&Function.identity/1)
+    |> Enum.map(&encode_chunks/1)
+    |> Enum.join()
   end
+
+  @spec encode_chunks([String.t()]) :: [String.t()]
+  defp encode_chunks([chunk]), do: chunk
+
+  @spec encode_chunks([String.t()]) :: [String.t()]
+  defp encode_chunks(chunks), do: to_string(Enum.count(chunks)) <> List.first(chunks)
 
   @spec decode(String.t()) :: String.t()
   def decode(string) do
+    string
+    |> String.split(~r/(\d*)[[:alpha:]]/, include_captures: true, trim: true)
+    |> Enum.map(&decode_chunks/1)
+    |> Enum.join()
+  end
+
+  @spec encode_chunks([String.t()]) :: [String.t()]
+  def decode_chunks(chunks) do
+    case Integer.parse(chunks) do
+      :error ->
+        chunks
+
+      {count, letter} ->
+        String.duplicate(letter, count)
+    end
   end
 end
