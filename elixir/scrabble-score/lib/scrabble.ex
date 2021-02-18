e2c7eee80c6a42dea2432a314c0891d77f06c061
Devin Brown <devin.g.brown@gmail.com>
2021-02-18 08:04:09 -0700

    Fucking submodule bull shit

---
+++ elixir/scrabble-score/lib/scrabble.ex
@@ -0,0 +1,40 @@
+defmodule Scrabble do
+  @scores %{
+    "A" => 1,
+    "B" => 3,
+    "C" => 3,
+    "D" => 2,
+    "E" => 1,
+    "F" => 4,
+    "G" => 2,
+    "H" => 4,
+    "I" => 1,
+    "J" => 8,
+    "K" => 5,
+    "L" => 1,
+    "M" => 3,
+    "N" => 1,
+    "O" => 1,
+    "P" => 3,
+    "Q" => 10,
+    "R" => 1,
+    "S" => 1,
+    "T" => 1,
+    "U" => 1,
+    "V" => 4,
+    "W" => 4,
+    "X" => 8,
+    "Y" => 4,
+    "Z" => 10
+  }
+
+  @spec score(String.t()) :: non_neg_integer
+  def score(word) do
+    word
+    |> String.trim()
+    |> String.graphemes()
+    |> Enum.map(&String.upcase/1)
+    |> Enum.map(&(@scores[&1]))
+    |> Enum.sum()
+  end
+end
