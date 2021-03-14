9ca986f42353c1d16ef71312fc7d0807b33f7855
Devin Brown <devin.g.brown@gmail.com>
2021-03-14 13:07:42 -0600

    tighten up the Typespecs

--- elixir/triangle/lib/triangle.ex
+++ elixir/triangle/lib/triangle.ex
@@ -1,16 +1,15 @@
 defmodule Triangle do
   @type kind :: :equilateral | :isosceles | :scalene
 
+  @spec kind(number, number, number) :: {:error, String.t()}
   def kind(a, b, c) when a <= 0 or b <= 0 or c <= 0,
     do: {:error, "all side lengths must be positive"}
 
+  @spec kind(number, number, number) :: {:error, String.t()}
   def kind(a, b, c) when a + b <= c or a + c <= b or b + c <= a,
     do: {:error, "side lengths violate triangle inequality"}
 
-  @doc """
-  Return the kind of triangle of a triangle with 'a', 'b' and 'c' as lengths.
-  """
-  @spec kind(number, number, number) :: {:ok, kind} | {:error, String.t()}
+  @spec kind(number, number, number) :: {:ok, kind}
   def kind(a, b, c) do
     case Enum.count(Enum.uniq([a, b, c])) do
       1 -> {:ok, :equilateral}
